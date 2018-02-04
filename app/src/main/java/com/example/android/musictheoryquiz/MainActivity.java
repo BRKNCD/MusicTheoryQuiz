b6bf51b4bf8e36fc58399f7ac65bcf8ce003b088
Gian <gianlupds@gmail.com>
2018-02-04 10:25:42 +0100

    Implemented a method to defocus EditText and hide the keyboard

--- app/src/main/java/com/example/android/musictheoryquiz/MainActivity.java
+++ app/src/main/java/com/example/android/musictheoryquiz/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.musictheoryquiz;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -277,4 +280,20 @@ public class MainActivity extends AppCompatActivity {
                 checkBoxArray[questionIndex][i].setChecked(false);
         }
     }
+
+    // REMOVE FOCUS AND HIDE KEYBOARD AFTER TOUCHING ANYWHERE ELSE THAN EDITTEXT
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View view = getCurrentFocus();
+        if (view != null && (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE) && view instanceof EditText && !view.getClass().getName().startsWith("android.webkit.")) {
+            int scrcoords[] = new int[2];
+            view.getLocationOnScreen(scrcoords);
+            float x = ev.getRawX() + view.getLeft() - scrcoords[0];
+            float y = ev.getRawY() + view.getTop() - scrcoords[1];
+            if (x < view.getLeft() || x > view.getRight() || y < view.getTop() || y > view.getBottom())
+                ((InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow((this.getWindow().getDecorView().getApplicationWindowToken()), 0);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
 }
